234fa7d00d1f2a594f2114f87a7e2e036cf81f13
Vinicius Menezes <viny2999@gmail.com>
2020-04-13 20:17:09 -0300

    fix

--- src/main/java/br/radixeng/service/DijkstraService.java
+++ src/main/java/br/radixeng/service/DijkstraService.java
@@ -63,43 +63,44 @@ public class DijkstraService {
     private List<Vertice> minimumRoute(Grafo grafo, Vertice v1, Vertice v2) {
         menorCaminho.add(v1);
 
-        for (int i = 0; i < grafo.getVertices().size(); i++) {
-            if (grafo.getVertices().get(i).getDescricao().equals(v1.getDescricao())) {
-                grafo.getVertices().get(i).setDistancia(0);
-            } else {
-                grafo.getVertices().get(i).setDistancia(9999);
-            }
-            this.naoVisitados.add(grafo.getVertices().get(i));
-        }
+		for (int i = 0; i < grafo.getVertices().size(); i++) {
+			if (grafo.getVertices().get(i).getDescricao().equals(v1.getDescricao())) {
+				grafo.getVertices().get(i).setDistancia(0);
+			} else {
+				grafo.getVertices().get(i).setDistancia(9999);
+			}
+			this.naoVisitados.add(grafo.getVertices().get(i));
+		}
 
-        Collections.sort(naoVisitados);
+		Collections.sort(naoVisitados);
 
-        while (!this.naoVisitados.isEmpty()) {
+		while (!this.naoVisitados.isEmpty()) {
             atual = this.naoVisitados.get(0);
-            for (int i = 0; i < atual.getArestas().size(); i++) {
-                vizinho = atual.getArestas().get(i).getDestino();
-                if (!vizinho.verificarVisita()) {
-                    if (vizinho.getDistancia() > (atual.getDistancia() + atual.getArestas().get(i).getPeso())) {
-                        vizinho.setDistancia(atual.getDistancia() + atual.getArestas().get(i).getPeso());
-                        vizinho.setPai(atual);
+			for (int i = 0; i < atual.getArestas().size(); i++) {
+				vizinho = atual.getArestas().get(i).getDestino();
+				if (!vizinho.verificarVisita()) {
+					if (vizinho.getDistancia() > (atual.getDistancia() + atual.getArestas().get(i).getPeso())) {
+						vizinho.setDistancia(atual.getDistancia() + atual.getArestas().get(i).getPeso());
+						vizinho.setPai(atual);
 
-                        if (vizinho == v2) {
-                            menorCaminho.clear();
-                            verticeCaminho = vizinho;
-                            menorCaminho.add(vizinho);
-                            while (verticeCaminho.getPai() != null) {
-                                menorCaminho.add(verticeCaminho.getPai());
-                                verticeCaminho = verticeCaminho.getPai();
-                            }
-                            Collections.sort(menorCaminho);
-                        }
-                    }
-                }
-            }
-            atual.visitar();
-            Collections.sort(naoVisitados);
-        }
-        return menorCaminho;
+						if (vizinho == v2) {
+							menorCaminho.clear();
+							verticeCaminho = vizinho;
+							menorCaminho.add(vizinho);
+							while (verticeCaminho.getPai() != null) {
+								menorCaminho.add(verticeCaminho.getPai());
+								verticeCaminho = verticeCaminho.getPai();
+							}
+							Collections.sort(menorCaminho);
+						}
+					}
+				}
+			}
+			atual.visitar();
+			this.naoVisitados.remove(atual);
+			Collections.sort(naoVisitados);
+		}
+		return menorCaminho;
     }
 
     private String mountResponse(int distance, List<String> paths) {
